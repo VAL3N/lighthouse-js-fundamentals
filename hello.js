d295bf87d4d5b9b3d97c73c4ca42cb5bd6ce5b12
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-05-07 02:22:30 +0000

    hello world for various names

---
+++ hello.js
@@ -0,0 +1,19 @@
+/*const sayHello  = function () {
+  console.log("Hello, world!");
+}
+sayHello();  */
+
+
+// arrow function
+/*const sayHello  = () => {
+  console.log("Hello, world!");
+}
+sayHello();  */
+
+//
+const sayHello  = function (name) {
+  console.log("Hello, " + name + "!");
+}
+
+sayHello("Felipe");
+sayHello("Kaylee");
