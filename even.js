9a9e1cf179822a8c223b33b27b793db7328e4457
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-05-07 02:38:21 +0000

    checks for even and odd numbers
    f
    f

---
+++ even.js
@@ -0,0 +1,11 @@
+
+// function that evalutates whether a number is even or odd (true or false)
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
